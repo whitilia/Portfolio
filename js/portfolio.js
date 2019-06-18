735efa52b1c87c1511c7addefabce4ecaa674d96
heekweon kang <heekweonkang@heekweonui-MacBook-Pro.local>
2019-06-18 20:39:22 +0900

    스크립트 수정

--- js/portfolio.js
+++ js/portfolio.js
@@ -6,6 +6,10 @@ $(function(){
         $(".section04").addClass('none');
     });
 
+    $(".section01 .logo").click(function(){
+        var offset = $(".section01").offset();
+        $('html, body').animate({scrollTop : offset.top}, 1000);
+    });
     $(".section01 .scroll").click(function(){
         var offset = $(".section02").offset();
         $('html, body').animate({scrollTop : offset.top}, 1000);
@@ -18,12 +22,16 @@ $(function(){
 
     $(".section03 .works_list li").click(function(){
         var index = $(".section03 .works_list li").index(this) + 1;
-        $(".section03 .work_detail .detail_area").removeClass('active');
-        $(".section03 .work_detail .detail_area.list" + index).addClass('active');
-
         var page_w = $("html").width();
         if(page_w <= 640){
-            $(".section03 .work_detail").appendTo(this);
+            $(".section03 .works_list li .work_detail").remove();
+            $(".section03 .work_detail .detail_area").removeClass('active');
+            $(".section03 .work_detail").clone().addClass('w640').appendTo(this);
+            $(".section03 .work_detail.w640 .detail_area.list" + index).addClass('active');
+        }else{
+            $(".section03 .works_list li .work_detail").remove();
+            $(".section03 .work_detail .detail_area").removeClass('active');
+            $(".section03 .work_detail .detail_area.list" + index).addClass('active');
         }
 
     });
